b76a50141a1d4dc856933eded5e6ffb7bb523305
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-03-18 22:51:31 +0530

    feat: integrated clerk auth

--- src/app/(marketing)/Header.tsx
+++ src/app/(marketing)/Header.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Header = () => {
   return (
     <header className="w-full h-20 border-b-2 border-slate-200 px-4">
-      Header
+      <UserButton afterSignOutUrl="/" />
     </header>
   );
 };
