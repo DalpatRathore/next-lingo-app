80d7e44059880053384df1e31cb497e31ca6d105
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-14 22:20:48 +0530

    feat: admin units crud ops

---
+++ src/app/admin/unit/create.tsx
@@ -0,0 +1,26 @@
+import {
+  Create,
+  Datagrid,
+  NumberInput,
+  ReferenceInput,
+  required,
+  SimpleForm,
+  TextField,
+  TextInput,
+} from "react-admin";
+
+export const UnitCreate = () => (
+  <Create>
+    <SimpleForm>
+      <TextInput source="title" validate={[required()]} label="Title" />
+      <TextInput
+        source="description"
+        validate={[required()]}
+        label="Description"
+      />
+      <ReferenceInput source="courseId" reference="courses"></ReferenceInput>
+
+      <NumberInput source="order" validate={[required()]} label="Order" />
+    </SimpleForm>
+  </Create>
+);
