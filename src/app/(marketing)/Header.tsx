d07bf1a87dd01393e5ab5f56a95111b9f81cf7b2
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-15 12:08:15 +0530

    feat: rename lingo to next lingo app

--- src/app/(marketing)/Header.tsx
+++ src/app/(marketing)/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src="/mascot.svg" height={40} width={40} alt="mascot"></Image>
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-            Lingo
+            Next Lingo App
           </h1>
         </div>
         <ClerkLoading>
