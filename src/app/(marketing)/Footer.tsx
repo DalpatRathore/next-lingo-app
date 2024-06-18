1718856da8c48c875dbb4f28e7aeb2f00622ef77
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-18 19:35:48 +0530

    style: footer button remove

--- src/app/(marketing)/Footer.tsx
+++ src/app/(marketing)/Footer.tsx
@@ -7,56 +7,46 @@ const Footer = () => {
   return (
     <footer className="w-full h-20 border-t-2 border-slate-200 p-2">
       <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full gap-3">
-        <Button size={"lg"} variant={"ghost"} className="w-full">
-          <Image
-            src={"/es.svg"}
-            alt="Spanish"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          ></Image>
-          Spanish
-        </Button>
-        <Button size={"lg"} variant={"ghost"} className="w-full">
-          <Image
-            src={"/hr.svg"}
-            alt="Croatian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          ></Image>
-          Croatian
-        </Button>
-        <Button size={"lg"} variant={"ghost"} className="w-full">
-          <Image
-            src={"/jp.svg"}
-            alt="Japanese"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          ></Image>
-          Japanese
-        </Button>
-        <Button size={"lg"} variant={"ghost"} className="w-full">
-          <Image
-            src={"/fr.svg"}
-            alt="France"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          ></Image>
-          France
-        </Button>
-        <Button size={"lg"} variant={"ghost"} className="w-full">
-          <Image
-            src={"/it.svg"}
-            alt="Italian"
-            height={32}
-            width={40}
-            className="mr-4 rounded-md"
-          ></Image>
-          Italian
-        </Button>
+        <Image
+          src={"/es.svg"}
+          alt="Spanish"
+          height={32}
+          width={40}
+          className="mr-4 rounded-md"
+        ></Image>
+
+        <Image
+          src={"/hr.svg"}
+          alt="Croatian"
+          height={32}
+          width={40}
+          className="mr-4 rounded-md"
+        ></Image>
+
+        <Image
+          src={"/jp.svg"}
+          alt="Japanese"
+          height={32}
+          width={40}
+          className="mr-4 rounded-md"
+        ></Image>
+
+        <Image
+          src={"/fr.svg"}
+          alt="France"
+          height={32}
+          width={40}
+          className="mr-4 rounded-md"
+        ></Image>
+
+        <Image
+          src={"/it.svg"}
+          alt="Italian"
+          height={32}
+          width={40}
+          className="mr-4 rounded-md"
+        ></Image>
+
         <Administer></Administer>
       </div>
     </footer>
